2238c071699b978af7df73486f6d10e3875c2701
WilderSturm <142720467+WilderSturm@users.noreply.github.com>
2023-09-09 13:48:45 +0700

    Add choice randomizer

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-console.log("hi");
+function getComputerChoice() {
+    let chooseOption = Math.floor(Math.random()*3+1);
+    if (chooseOption === 1) {
+        return "Rock";
+    } else if (chooseOption === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+getComputerChoice();
